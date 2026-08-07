b3568fcf230f69befa595381d390554b202440fe
Satvir <sssatvir12@gmail.com>
2026-08-07 14:27:10 +0530

    Add hello_world.py example using StateGraph
    This introduces a simple example demonstrating the usage of StateGraph with "hello" and "goodbye" nodes. The graph processes a stateful message and outputs the result after traversal.

---
+++ basic/hello_world.py
@@ -0,0 +1,27 @@
+from langgraph.graph import START,END,StateGraph
+from util.langgraph_util import display
+from typing import TypedDict
+
+class HelloWorldState(TypedDict):
+    message: str
+
+def hello(state: HelloWorldState):
+    print(f"Hello  {state['message']}")
+    return {'message': f"Hello {state['message']} again!"}
+def goodbye(state: HelloWorldState):
+    print(f"Goodbye ! {state['message']}")
+    return {'message': f"Goodbye {state['message']} again!"}
+
+
+
+graph = StateGraph(HelloWorldState)
+graph.add_node("hello",hello)
+graph.add_node("bye", goodbye)
+
+graph.add_edge(START,"hello")
+graph.add_edge("hello","bye")
+graph.add_edge("bye",END)
+
+runnable = graph.compile()
+out=runnable.invoke({"message":"satvir"})
+print(out)
