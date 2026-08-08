0cd4a939d6a18fd842e05a3d4977c1bf3a9a07ed
Satvir <sssatvir12@gmail.com>
2026-08-08 12:28:28 +0530

    Refactor chatbot reducer for consistency and compactness
    Consolidated code layout by removing extraneous spacing and adopting a cleaner format. Renamed `ChatbotState` to `chatbot` for naming consistency and streamlined function definitions for improved readability. Minor adjustments in edge declarations and invocation output handling were also made.

--- reducers/reducerdemo.py
+++ reducers/reducerdemo.py
@@ -1,50 +1,32 @@
-from langgraph.graph import START, END, StateGraph
+from langgraph.graph import START,END,StateGraph
 from util.langgraph_util import display
-from typing import TypedDict, Annotated
+from typing import TypedDict,Annotated
 from operator import add
-
 from langchain_core.messages import AnyMessage
-from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.messages import AIMessage,HumanMessage
+
+class chatbot(TypedDict):
+    message: Annotated[list[AnyMessage],add]
+    discount: Annotated[int,add]
+
+def connect_to_sale(state: chatbot):
+    return {"message":[AIMessage(content="connect to sale")],"discount":10}
+def sale_team_response(state: chatbot):
+    return {"message":[AIMessage(content="offer for you")],"discount":20}
 
 
-class ChatbotState(TypedDict):
-    message: Annotated[list[AnyMessage], add]
-    discount: Annotated[int, add]
 
+graph = StateGraph(chatbot)
+graph.add_node("connect_to_sale",connect_to_sale)
+graph.add_node("sale_team_response",sale_team_response)
 
-def connect_to_sale(state: ChatbotState):
-    return {
-        "message": [AIMessage(content="connect to sale")],
-        "discount": 10
-    }
-
-
-def sale_team_response(state: ChatbotState):
-    return {
-        "message": [AIMessage(content="offer for you")],
-        "discount": 20
-    }
-
-
-graph = StateGraph(ChatbotState)
-
-graph.add_node("connect_to_sale", connect_to_sale)
-graph.add_node("sale_team_response", sale_team_response)
-
-graph.add_edge(START, "connect_to_sale")
-graph.add_edge("connect_to_sale", "sale_team_response")
-graph.add_edge("sale_team_response", END)
+graph.add_edge(START,"connect_to_sale")
+graph.add_edge("connect_to_sale","sale_team_response")
+graph.add_edge("sale_team_response",END)
 
 chatbot = graph.compile()
-
 display(chatbot)
-
-result = chatbot.invoke({
-    "message": [HumanMessage(content="i want to buy product")],
-    "discount": 0
-})
-
-for m in result["message"]:
-    print(f"message -> {m.content}")
-
-print(f"total discount is {result['discount']}")
+message=chatbot.invoke({"message":[HumanMessage(content="i want to buy product")],"discount":0})
+for m in message['message']:
+    print(f"you have discount off->{m}")
+print(f"total discount is {message['discount']}")
