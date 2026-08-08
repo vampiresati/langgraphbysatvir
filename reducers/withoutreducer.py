93e61d86905d3c19f8075e4e4e76cd48d336151d
Satvir <sssatvir12@gmail.com>
2026-08-08 12:04:54 +0530

    Add initial chatbot state graph implementation
    Introduces a state graph for a chatbot with nodes, edges, and transitions. Implements basic functionality to simulate a sales interaction, including discount calculations and message flow. This setup lays the foundation for future enhancements.

---
+++ reducers/withoutreducer.py
@@ -0,0 +1,31 @@
+from langgraph.graph import START,END,StateGraph
+from util.langgraph_util import display
+from typing import TypedDict,Annotated
+from langchain_core.messages import AnyMessage
+from langchain_core.messages import AIMessage,HumanMessage
+
+class chatbot(TypedDict):
+    message: list[AnyMessage]
+    discount: int
+
+def connect_to_sale(state: chatbot):
+    return {"message":AIMessage(content="connect to sale"),"discount":10}
+def sale_team_response(state: chatbot):
+    return {"message":AIMessage(content="offer for you"),"discount":20}
+
+
+
+graph = StateGraph(chatbot)
+graph.add_node("connect_to_sale",connect_to_sale)
+graph.add_node("sale_team_response",sale_team_response)
+
+graph.add_edge(START,"connect_to_sale")
+graph.add_edge("connect_to_sale","sale_team_response")
+graph.add_edge("sale_team_response",END)
+
+chatbot = graph.compile()
+display(chatbot)
+message=chatbot.invoke({"message":HumanMessage(content="i want to buy product")})
+for m in message['message']:
+    print(f"you have discount off->{m}")
+print(f"total discount is {message['discount']}")
